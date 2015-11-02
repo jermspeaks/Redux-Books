79fbde04a567213c6e11d7ac2fb4d72d963a8083
Jeremy Wong <jermspeaks@gmail.com>
2015-11-01 18:04:01 -0800

    editing books, oh yeah

--- reducers/books.js
+++ reducers/books.js
@@ -6,10 +6,15 @@ export default function books(state = initialState, action) {
   switch(action.type) {
     case types.ADD_BOOK:
       return [...state, {
-        // TODO still feels like a code smell
         id: action.book.id,
         title: action.book.title
       }];
+    case types.EDIT_BOOK:
+      return state.map((book) =>
+        book.id === action.id ?
+          Object.assign({}, book, { title: action.title }) :
+          book
+      )
     case types.DELETE_BOOK:
       return state.filter((book) => {
         return book.id !== action.id
--- test/reducers/books.spec.js
+++ test/reducers/books.spec.js
@@ -25,6 +25,21 @@ describe('books reducer', () => {
     ).toEqual([testBook]);
   });
 
+  it('should edit a book from the book list', () => {
+    const newBook = {
+      id: 0,
+      title: 'Little House on the Prairie'
+    }
+
+    expect(
+      reducer(oneBookState, {
+        type: types.EDIT_BOOK,
+        id: newBook.id,
+        title: newBook.title
+      })
+    ).toEqual([newBook]);
+  })
+
   it('should delete a book from the book list', () => {
     expect(
       reducer(oneBookState, {
